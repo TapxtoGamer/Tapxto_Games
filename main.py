5e9253affb6fe0d094f8f6b2c615aa89756e8544
Tiago Peixoto <tapxto@gmail.com>
2020-10-31 18:59:40 +0000

    Adiciona banner ao menu inicial

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import game_utils
 import casa_fantasma
 import adivinha_numero
 
+def mostra_banner():
+    print("""
+  ______                 __           ______                         
+ /_  __/___ _____  _  __/ /_____     / ____/___ _____ ___  ___  _____
+  / / / __ `/ __ \| |/_/ __/ __ \   / / __/ __ `/ __ `__ \/ _ \/ ___/
+ / / / /_/ / /_/ />  </ /_/ /_/ /  / /_/ / /_/ / / / / / /  __(__  ) 
+/_/  \__,_/ .___/_/|_|\__/\____/   \____/\__,_/_/ /_/ /_/\___/____/  
+         /_/                                                         
 
+    """)
+
+mostra_banner()
 opcao = None
 print('Jogos disponívels:')
 print('   - 1: Adivinha o número')
